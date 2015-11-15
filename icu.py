6fe79873c7865857c2c361837414864bfdfc9164
Ben <btkaija@gmail.com>
2015-11-15 17:57:32 +0000

    final checking for events added

--- icu.py
+++ icu.py
@@ -20,7 +20,9 @@ def get_times(tle, elevation, location):
     viewer.horizon=horizon
     i = 0
     times = []
-    while i < 100:
+
+    #get 50 times to view satellite
+    while i < 50:
         sat.compute(viewer)
         np = viewer.next_pass(sat)
         next_pass_times = np[::2]
@@ -62,19 +64,15 @@ def led_and_sound_sec():
     GPIO.output(25, False)
     
 
-def is_night(zipcode, date_time):
-    zip_request = 'http://www.weather.gov/forecasts/xml/sample_products/browser_interface/ndfdXMLclient.php?listZipCodeList='
-   
-    r = requests.get(zip_request+str(zipcode))
-    lat_lon = (ET.fromstring(r.text)[0].text).split(',')
-    latitude = lat_lon[0]
-    longitude = lat_lon[1]
-    
+#will return true of false based on if the time is considered night
+def is_night(latitude, longitude, date_time):
+        
     #separate date from time here
     date_time = date_time.split('T')
     #array of time to compare to result
     time_val = time.mktime(time.strptime('1999-'+date_time[1], '%Y-%H:%M:%S'))
     time_val = datetime.fromtimestamp(time_val)
+    
     time_val = time_val.hour*60*60 + time_val.minute*60 + time_val.second
     date = date_time[0]
 
@@ -86,33 +84,25 @@ def is_night(zipcode, date_time):
     #formatting result
     sunrise = result['results']['sunrise']
     sunset = result['results']['sunset']
-    
+    #print ''
+    #print date_time, sunrise, sunset
     sunrise = time.mktime(time.strptime('1999-'+sunrise, '%Y-%I:%M:%S %p'))
     sunset = time.mktime(time.strptime('1999-'+sunset, '%Y-%I:%M:%S %p'))
-    sunrise = datetime.fromtimestamp(sunrise) - timedelta(hours=5)+timedelta(hours=1)
-    sunset = datetime.fromtimestamp(sunset) - timedelta(hours=5)-timedelta(hours=1)
+    sunrise = datetime.fromtimestamp(sunrise) - timedelta(hours=1)
+    sunset = datetime.fromtimestamp(sunset) + timedelta(hours=1)
     sunrise = sunrise.hour*60*60 + sunrise.minute*60 + sunrise.second
     sunset = sunset.hour*60*60 + sunset.minute*60 + sunset.second
-    
     if(time_val>sunrise and time_val<sunset):
         return False
     else:
         return True
     
 
-def is_weather_clear(zipcode, date_time):
-    zip_request = 'http://www.weather.gov/forecasts/xml/sample_products/browser_interface/ndfdXMLclient.php?listZipCodeList='
+def is_weather_clear(latitude, longitude, date_time):
     sky_request = 'http://www.weather.gov/forecasts/xml/sample_products/browser_interface/ndfdXMLclient.php?&featureType=Forecast_Gml2Point&propertyName=sky'
 
-    r = requests.get(zip_request+str(zipcode))
-    lat_lon = (ET.fromstring(r.text)[0].text).split(',')
-    latitude = lat_lon[0]
-    longitude = lat_lon[1]
-    #print 'Zipcode located at LAT:',latitude,'LON:',longitude
-    
     lat_lon_req = '&gmlListLatLon='+latitude+','+longitude
     
-    #for loop here through a range of times from start_time to stop_time
     #YYYY-MM-DDTHH:MM:SS format
     #datetime = '2015-11-15T12:00:00'
     
@@ -122,14 +112,14 @@ def is_weather_clear(zipcode, date_time):
     try:
         cover = int((ET.fromstring(r.text)[1][0][2].text).split('.')[0])
     except:
-        print 'Error retrieving weather data for date_time.'
-        print r.text
-        cover = 100
+        #print 'Error retrieving weather data for'+date_time
+        #print r.text
+        cover = 101
 
     if cover < 25:
-        return True
+        return [True, cover]
     else:
-        return False
+        return [False, cover]
 
 def send_text_msg(msg):
     email_user = 'btkaija@gmail.com'
@@ -163,24 +153,64 @@ for i in arg1:
         zip_code = i[1]
     else:
         satellite = i[1]
+
 result = get_tle(satellite)
+
+print ''
+print "Satellite Info: "+result
+
 result = result.split('\n')
 geolocator = Nominatim()
 location = geolocator.geocode(zip_code + ", United States")
 g = geocoder.elevation((location.latitude, location.longitude))
+
+
 times = get_times(result, g.meters, location)
 #print times
-
 clear_times = []
 
+print 'NOTE: all times are in UTC time'
+print ''
+
+#loop through each time found and check to see if visible
 for t in times:
-    clear = is_weather_clear(zip_code, t)
-    night = is_night(zip_code, t)
-    
-    if(clear and night):
-        print 'Checking time: '+t
+    clear = is_weather_clear(str(location.latitude),str(location.longitude), t)
+    night = is_night(str(location.latitude),str(location.longitude), t)
+    if(clear[0] and night):
+        print 'Clear time found! --> '+t
+        print 'The cloud cover is will only be at '+str(clear[1])+'%'
         clear_times.append(t)
         if(len(clear_times)==5):
             break
 
+print ''
+print 'Viewable events show below'
 print clear_times
+print ''
+print 'Begining to wait for the viewable events listed above'
+print ''
+#enter loop waiting for events to occur
+
+event_num = 0
+
+while(1):
+    now = datetime.utcnow()
+    new_event = clear_times[event_num]
+
+    check_time = time.mktime(time.strptime(new_event, '%Y-%m-%dT%H:%M:%S'))
+    check_time = datetime.fromtimestamp(check_time) - timedelta(seconds=(15*60))
+    #compare now, check_time to see if event will occur
+    if(now>check_time):
+        print 'Viewable event is occurring soon! (15 min from now: '+str(now)+')'
+        event_num = event_num+1
+        if event_num == len(clear_times):
+            print 'All events have occured for the requested satellite.'
+            break
+        #send notifications
+        led_and_sound_sec()
+        send_text_msg('A viewable event for the satellite with id: '+satellite+'''
+                      is occurring in the zip code: '''+zip_code+' in 15 minutes from '+str(now))
+
+    #wait for 10 seconds before checking the time again
+    time.sleep(10)
+#end
